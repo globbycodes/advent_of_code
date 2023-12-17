c27f5b1c4ff99d5d5d33e3d983c504e72a95df51
globbycodes <shawnsaliyev@gmail.com>
2023-12-16 20:54:55 -0600

    day 3 part 2 changes

--- java/src/problems/Problem3.java
+++ java/src/problems/Problem3.java
@@ -22,17 +22,17 @@ public class Problem3 extends RootProblem{
 
         switch (partNumber) {
             case 1:
-                partOne(input);
+                partOne();
                 break;
             case 2:
-                partTwo(input);
+                partTwo();
                 break;
             default:
                 return;
         }
     }
 
-    private void partOne(String[] input){
+    private void partOne(){
         int sum = 0;
         StringBuilder numberString = new StringBuilder();
         List<int[]> numberIndices = new ArrayList<>();
@@ -47,13 +47,11 @@ public class Problem3 extends RootProblem{
 
             numberString.setLength(0);
             int startIndex = -1, endIndex = -1;
-            // System.out.println("gggg"+line);
             for(int j = 0; j < width; j++){
                 
                 char curChar = line.charAt(j);
-                int [] indices = {startIndex, endIndex};
-                if(endIndex >= 0 && (!isNumber(curChar) || j == width - 1)){
-                    // System.out.println("the char: " + curChar);
+                int[] indices = {startIndex, endIndex};
+                if(endIndex >= 0 && (!isNumber(curChar))){
                     numberIndices.add(indices);
                     numbers.add(Integer.parseInt(numberString.toString()));
                     numberString.setLength(0);
@@ -67,6 +65,8 @@ public class Problem3 extends RootProblem{
                     }
                     endIndex = j;
                     numberString.append(curChar);
+                    indices[0] = startIndex;
+                    indices[1] = endIndex;
                     if(j == width - 1){
                         numberIndices.add(indices);
                         numbers.add(Integer.parseInt(numberString.toString()));
@@ -74,15 +74,12 @@ public class Problem3 extends RootProblem{
                 }
 
             }
-
             sum+= countPartNumbers(numberIndices, numbers, i);
-
         }
         TerminalPrint.printAnswerMsg(problemNumber, 1, sum);
     }
 
     private int countPartNumbers(List<int[]> indices, List<Integer> numbers, int lineNumber){
-        System.out.println(numbers);
         int sum = 0;
         String line = input[lineNumber];
         String upperLine = lineNumber > 0 ? input[lineNumber - 1] : null;
@@ -94,25 +91,21 @@ public class Problem3 extends RootProblem{
         // check current row
         for(int i = 0; i < indices.size(); i++){
             
-            System.out.println(numbers.get(i));
             startIndex = indices.get(i)[0];
             endIndex = indices.get(i)[1];
             
             if(startIndex > 0 && line.charAt(startIndex - 1) != '.'){
-                System.out.println("left neighbor");
                 sum+= numbers.get(i);
                 continue;
             }
 
             if(endIndex < width - 1 && line.charAt(endIndex + 1) != '.'){
-                System.out.println("right neighbor");
                 sum+= numbers.get(i);
                 continue;
             }
 
             if(upperLine != null){
                 if(checkSubString(upperLine, startIndex, endIndex)) {
-                    System.out.println("upperLine");
                     sum+= numbers.get(i);
                     continue;
                 }
@@ -120,7 +113,6 @@ public class Problem3 extends RootProblem{
 
             if(lowerLine != null){
                 if(checkSubString(lowerLine, startIndex, endIndex)){
-                    System.out.println("lowerLine");
                     sum+= numbers.get(i);
                     continue;
                 }
@@ -134,10 +126,8 @@ public class Problem3 extends RootProblem{
     private boolean checkSubString(String line, int startIndex, int endIndex){
         char curChar;
         if(startIndex > 0){
-            // System.out.println(startIndex);
             curChar = line.charAt(startIndex - 1);
             if(!isNumber(curChar) && curChar != '.'){
-                System.out.println("diag" + startIndex);
                 return true;
             }
         }
@@ -145,7 +135,6 @@ public class Problem3 extends RootProblem{
         if(endIndex < width - 1){
             curChar = line.charAt(endIndex + 1);
             if(!isNumber(curChar) && curChar != '.'){
-                System.out.println("diag" + endIndex);
                 return true;
             }
         }
@@ -153,7 +142,6 @@ public class Problem3 extends RootProblem{
         for(int i = startIndex; i <= endIndex; i++){
             curChar = line.charAt(i);
             if(!isNumber(curChar) && curChar != '.'){
-                System.out.println("vert" + endIndex);
                 return true;
             }
         }
@@ -167,7 +155,134 @@ public class Problem3 extends RootProblem{
         return false;
     }
 
-    private void partTwo(String[] input){
+    private void partTwo(){
+        int sum = 0;
+        String curLine;
+        for(int i = 0; i < input.length; i++){
+            curLine = input[i];
+            
+            for(int j = 0; j < width; j++){
+                char curChar = curLine.charAt(j);
 
+                if(curChar == '*'){
+                    sum += searchAroundGear(i,j);
+                }
+            }
+        }
+        TerminalPrint.printAnswerMsg(problemNumber, 2, sum);
+    }
+
+    private int searchAroundGear(int lineNumber, int gearIndex){
+        int sum = 0;
+
+        int num1;
+        int num2;
+
+        int numberCount = 0;
+        int index;
+        
+        char symbol;
+        StringBuilder number = new StringBuilder();
+        String numberToConvert;
+        
+        // Check left from the gear
+        if(gearIndex > 0){
+            index = gearIndex - 1; // one position to the left of the gear
+            symbol = input[lineNumber].charAt(index);
+
+            number = buildNumber(input[lineNumber], index, number, false);
+            if(number.length() > 0){
+                numberToConvert = number.toString();
+                System.out.println(numberToConvert + " left");
+                num1 = Integer.parseInt(numberToConvert);
+                number.setLength(0);
+                numberCount++;
+            }
+        }
+
+        // Check right from the gear
+        if(gearIndex < width - 1){
+            index = gearIndex + 1; // one position to the right of the gear
+            symbol = input[lineNumber].charAt(index);
+            number = buildNumber(input[lineNumber], index, number, true);
+            if(number.length() > 0){
+                numberToConvert = number.toString();
+                System.out.println(numberToConvert + " right");
+                if(numberCount == 0){
+                    num1 = Integer.parseInt(numberToConvert);
+                }else{
+                    num2 = Integer.parseInt(numberToConvert);
+                }
+                numberCount++;
+            }
+        }
+
+        String checkLine;
+        // Check above the gear
+        if(lineNumber > 0){
+            checkLine = input[lineNumber - 1];
+
+        }
+        return 0;
+    }
+
+    private int[] checkLine(int lineNumber, int gearIndex){
+        String curLine = input[lineNumber];
+        int[] localResult = new int[] {-1, -1};
+        int startIndex = gearIndex - 1 > 0 ? gearIndex - 1 : gearIndex; // if not at the beginning of the line, go one position to the left
+        int endIndex = gearIndex + 1 < width - 1 ? gearIndex + 1 : gearIndex;
+        StringBuilder number = new StringBuilder();
+        String numberToConvert;
+
+
+
+        for(int i = startIndex; i <= endIndex; i++){
+            if(isNumber(curLine.charAt(i))){
+                int curIndex = i;
+                if(curIndex == gearIndex - 1){
+                    // duplicated while loops -> extract into separate methods
+                    number = buildNumber(curLine, curIndex, number, false);
+                    if(number.length() > 0){
+                        number = number.reverse();
+                    }
+                }else{
+                    if(number.length() > 0 && curIndex == gearIndex){
+                        number = buildNumber(curLine, curIndex, number, true);
+                    }else if(number.length() > 0){
+                        // Found a second number
+                    }
+                }
+            }
+        }
+        return localResult;
+    }
+    
+    private StringBuilder buildNumber(String curLine, int curIndex, StringBuilder number, boolean dir){
+        
+        int increment = dir ? 1 : -1; // if left then decrement by 1
+        char curSymbol = curLine.charAt(curIndex);
+        while(bound(curIndex, dir) && isNumber(curSymbol)){
+            number.append(curSymbol);
+            curIndex += increment;
+            if(bound(curIndex, dir)){
+                curSymbol = curLine.charAt(curIndex);
+            }else{
+                break;
+            }
+        }
+
+        if(!dir) number = number.reverse();
+
+        return number;
+    }
+
+    private boolean bound(int curIndex, boolean dir){
+        boolean result;
+        if(!dir){
+            result = curIndex >= 0;
+        }else{
+            result = curIndex <= width - 1;
+        }
+        return result;
     }
 }
